7325169c8d013a719cc0149807f63bbe3922b696
Aponovi <elfi.krupsky@gmail.com>
2022-05-30 18:59:28 +0200

    fix issue #1

---
+++ tests/units/mocks.py
@@ -0,0 +1,35 @@
+def mock_loadClubs():
+    clubs = [
+        {
+            "name": "Test Club",
+            "email": "test@club.com",
+            "points": "13"
+        },
+        {
+            "name": "Second Test Club",
+            "email": "secondtest@club.com",
+            "points": "50"
+        },
+        {
+            "name": "Last Test Club",
+            "email": "lasttest@club.com",
+            "points": "2"
+        },
+    ]
+    return clubs
+
+
+def mock_loadCompetitions():
+    competitions = [
+        {
+            "name": "Spring Festival",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Fall Classic",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "13"
+        },
+    ]
+    return competitions
